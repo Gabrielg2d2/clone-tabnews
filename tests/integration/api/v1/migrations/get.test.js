f417e696b98d86f6e76433e9e028f3f2bce97b7e
G2D2 <101529710+Gabrielg2d2@users.noreply.github.com>
2024-07-22 22:14:06 +0000

    test: 💍 get and post migrations

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,18 +1,12 @@
+import { database } from 'infra/database'
+
 test('GET to /api/v1/migrations/get, should return 200', async () => {
     const response = await fetch('http://localhost:3000/api/v1/migrations')
 
     expect(response.status).toBe(200)
 
     const data = await response.json()
-    console.log(data)
-
-    // [
-    //     {
-    //         path: 'infra/migrations/1721596001832_test.js',
-    //         name: '1721596001832_test',
-    //         timestamp: 1721596001832
-    //     }
-    // ]
-
     expect(Array.isArray(data)).toBe(true)
+
+    expect(data.length).toBeGreaterThan(0)
 })
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,5 +1,11 @@
-test('GET to /api/v1/migrations/post, should return 200', async () => {
-    const response = await fetch('http://localhost:3000/api/v1/migrations')
+test('POST to /api/v1/migrations/get, should return 200', async () => {
+    const response = await fetch('http://localhost:3000/api/v1/migrations', {
+        method: 'POST'
+    })
 
     expect(response.status).toBe(200)
+
+    const data = await response.json()
+
+    expect(Array.isArray(data)).toBe(true)
 })
