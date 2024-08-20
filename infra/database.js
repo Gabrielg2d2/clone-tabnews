a633c63a1bc6f78e9fd7f6a87e5e298d9186b9bb
G2D2 <101529710+Gabrielg2d2@users.noreply.github.com>
2024-08-20 19:22:14 +0000

    refactor: 💡 getNewClient

--- infra/database.js
+++ infra/database.js
@@ -27,8 +27,8 @@ async function query(queryObject) {
 async function getNewClient() {
     const client = new Client({
         host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
         user: process.env.POSTGRES_USER,
+        port: process.env.POSTGRES_PORT,
         database: process.env.POSTGRES_DB,
         password: process.env.POSTGRES_PASSWORD,
         ssl: getSSLValues()
